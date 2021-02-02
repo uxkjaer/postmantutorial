31520a82454e89da733f1260c288f46fdeafd97c
Jakob <jakob.kjaer@health.qld.gov.au>
2021-02-02 13:15:18 +1000

    Initialize repository with easy-ui5

---
+++ uimodule/webapp/controller/MainView.controller.js
@@ -0,0 +1,7 @@
+sap.ui.define([
+  "qldh/postmanTut/controller/BaseController"
+], function(Controller) {
+  "use strict";
+
+  return Controller.extend("qldh.postmanTut.controller.MainView", {});
+});
