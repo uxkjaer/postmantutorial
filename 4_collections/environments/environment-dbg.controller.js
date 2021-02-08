f95b38d8eef0d10b03a3db69dcf75ec2ce13693d
Jakob <jakob.kjaer@health.qld.gov.au>
2021-02-08 15:38:06 +1000

    Updates

--- 4_collections/environments/environment-dbg.controller.js
+++ 4_collections/environments/environment-dbg.controller.js
@@ -1,37 +1,41 @@
+
 sap.ui.define(
-  ["luigi/demo/common/BaseController", 'luigi/demo/libs/luigi-client/luigi-client'],
-  function(Controller, JSONModel) {
+  ["luigi/demo/common/BaseController", 'luigi/demo/libs/luigi-client/luigi-client',     "/sap/ui/model/json/JSONModel",
+],
+  function(Controller,client, JSONModel) {
     'use strict';
 
-    return Controller.extend('qldh.environment.environment', {onValidate: function () {
-      var text = JSON.stringify({
-        userId: 1,
-        id: 1,
-        title:
-          "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-        body:
-          "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
-      });
-      var myText = this.getView().byId("myText").getValue();
-      try {
-        myText = JSON.stringify(JSON.parse(myText));
-      } catch (error) {
-        this.getView().byId("failure").setVisible(true);
-        return;
+    return Controller.extend('qldh.environment.environment', {
+      onInit: function(){
+        this.setModel(new JSONModel({
+          code: `
+          const auth = 'Basic ' + btoa(pm.environment.get("Username") + ":" + pm.environment.get("Password"));
+          headers['Authorization'] =  auth;
+          pm.environment.set('auth', auth);`,
+          test: `
+          pm.test("Work item is not Zero", function () {
+            var jsonData = pm.response.json();
+            pm.expect(jsonData.Workitem).not.eql("0000000000000");
+        });
+        `
+        
+        }));
+      },
+      onValidate: function () {
+        if (this.byId("2").getSelected() === true) {
+          
+          this.getView().byId("success").setVisible(true);
+          this.getView().byId("failure").setVisible(false);
+          this._showFireworks();
+          setTimeout(() => {
+            document.getElementById("myFrame").style.visibility = "visible"
+          }, 200)
+        } else {
+          this.getView().byId("failure").setVisible(true);
+          this.getView().byId("success").setVisible(false);
+        }
       }
-
-      if (myText === text) {
-        this.getView().byId("success").setVisible(true);
-        this.getView().byId("failure").setVisible(false);
-        this._showFireworks();
-        setTimeout(() => {
-          document.getElementById("content").classList.add("item-fade")
-        }, 200)
-      } else {
-        this.getView().byId("failure").setVisible(true);
-        this.getView().byId("success").setVisible(false);
-      }
-    }
   });
 }
 );
+
